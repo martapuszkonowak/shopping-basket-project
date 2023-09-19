4b3aaa90ae4ce3022b85b7d8a14d70383f5b12e4
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-09-19 21:22:58 +0100

    Created new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>Marta's Tech Shop</h1>
+      </div>
+      <div className="products"></div>
+    </div>
+  );
+};
