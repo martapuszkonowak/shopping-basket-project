e058691473c94b789d044249835bf16603606b7f
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-11-16 20:55:32 +0000

    Created shop-context.jsx

---
+++ src/context/shop-context.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+import { PRODUCTS } from "../products";
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
+
+export const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+  return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
+};
